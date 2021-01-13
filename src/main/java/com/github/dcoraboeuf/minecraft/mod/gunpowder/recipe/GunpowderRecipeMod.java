608685788aa3cd6757decc04aa5ded1d4af50a95
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2021-01-13 16:29:40 +0100

    Recipe

---
+++ src/main/java/com/github/dcoraboeuf/minecraft/mod/gunpowder/recipe/GunpowderRecipeMod.java
@@ -0,0 +1,11 @@
+package com.github.dcoraboeuf.minecraft.mod.gunpowder.recipe;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(GunpowderRecipeMod.MODID)
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+public class GunpowderRecipeMod {
+
+    public static final String MODID = "gunpowder-recipe";
+
+}
